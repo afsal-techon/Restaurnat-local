bcb0dfff2e6121320508d65e0208505a38689cb6
afsal-techon <techon.afsal@gmail.com>
2025-07-25 10:39:45 +0530

    done

--- server/uninstall.cjs
+++ server/uninstall.cjs
@@ -2,7 +2,7 @@ const path = require('path');
 const { Service } = require('node-windows');
 
 const svc = new Service({
-  name: 'MERN POS',
+  name: 'TEC POS',
   script: path.join(__dirname, 'server.cjs'), 
 });
 
